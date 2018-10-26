13624a2fc5bd58ad4c4ea073195de736cbd035fa
unknown <yangzd@chinabrake.com>
2018-10-26 13:19:05 +0800

    添加项目文件。

---
+++ Talk/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TxtIP = New System.Windows.Forms.TextBox()
+        Me.TxtPort = New System.Windows.Forms.TextBox()
+        Me.LBOnLine = New System.Windows.Forms.ListBox()
+        Me.TxtMsg = New System.Windows.Forms.RichTextBox()
+        Me.TxtSendMsg = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ButListen = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtIP
+        '
+        Me.TxtIP.Location = New System.Drawing.Point(53, 12)
+        Me.TxtIP.Name = "TxtIP"
+        Me.TxtIP.Size = New System.Drawing.Size(99, 21)
+        Me.TxtIP.TabIndex = 0
+        Me.TxtIP.Text = "192.168.12.114"
+        '
+        'TxtPort
+        '
+        Me.TxtPort.Location = New System.Drawing.Point(53, 38)
+        Me.TxtPort.Name = "TxtPort"
+        Me.TxtPort.Size = New System.Drawing.Size(99, 21)
+        Me.TxtPort.TabIndex = 0
+        Me.TxtPort.Text = "5555"
+        '
+        'LBOnLine
+        '
+        Me.LBOnLine.FormattingEnabled = True
+        Me.LBOnLine.ItemHeight = 12
+        Me.LBOnLine.Location = New System.Drawing.Point(425, 10)
+        Me.LBOnLine.Name = "LBOnLine"
+        Me.LBOnLine.Size = New System.Drawing.Size(215, 292)
+        Me.LBOnLine.TabIndex = 1
+        '
+        'TxtMsg
+        '
+        Me.TxtMsg.Location = New System.Drawing.Point(12, 66)
+        Me.TxtMsg.Name = "TxtMsg"
+        Me.TxtMsg.Size = New System.Drawing.Size(384, 250)
+        Me.TxtMsg.TabIndex = 2
+        Me.TxtMsg.Text = ""
+        '
+        'TxtSendMsg
+        '
+        Me.TxtSendMsg.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TxtSendMsg.Location = New System.Drawing.Point(12, 324)
+        Me.TxtSendMsg.Name = "TxtSendMsg"
+        Me.TxtSendMsg.Size = New System.Drawing.Size(309, 21)
+        Me.TxtSendMsg.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(327, 322)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(69, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "发送"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ButListen
+        '
+        Me.ButListen.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ButListen.ForeColor = System.Drawing.Color.Fuchsia
+        Me.ButListen.Location = New System.Drawing.Point(158, 12)
+        Me.ButListen.Name = "ButListen"
+        Me.ButListen.Size = New System.Drawing.Size(74, 47)
+        Me.ButListen.TabIndex = 5
+        Me.ButListen.Text = "开始监听"
+        Me.ButListen.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "IP:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "端口:"
+        '
+        'TextName
+        '
+        Me.TextName.Location = New System.Drawing.Point(241, 39)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(155, 21)
+        Me.TextName.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(338, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 27)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "置顶"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(241, 11)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 27)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "老板键"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(669, 30)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(65, 36)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "闪烁"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 349)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButListen)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TxtSendMsg)
+        Me.Controls.Add(Me.TxtMsg)
+        Me.Controls.Add(Me.LBOnLine)
+        Me.Controls.Add(Me.TxtPort)
+        Me.Controls.Add(Me.TxtIP)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "服务器 0.1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtIP As TextBox
+    Friend WithEvents TxtPort As TextBox
+    Friend WithEvents LBOnLine As ListBox
+    Friend WithEvents TxtMsg As RichTextBox
+    Friend WithEvents TxtSendMsg As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ButListen As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextName As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Talk/Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Threading
+Imports System.Net
+Imports System.Net.Sockets
+
+Public Class Form1
+    Dim ListenSocket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp) '创建监听的套接字
+    Dim Dict As New Dictionary(Of String, Socket) '用于保存连接的客户的套接字的键值对集合
+    Dim DictThre As New Dictionary(Of String, Thread) '用于保存通信线程的键值对集合
+    '判断窗口目前的显示或是隐藏的变量
+    Dim ShowOrHide As Integer = 0
+    '声明注册热键API函数
+    Public Declare Function RegisterHotKey Lib "user32" (ByVal hWnd As Integer, ByVal id As Integer, ByVal fsModifiers As Integer, ByVal vk As Integer) As Integer
+    '声明注销热键API函数
+    Public Declare Function UnregisterHotKey Lib "user32" (ByVal hWnd As Integer, ByVal id As Integer) As Integer
+    Public Const WM_HOTKEY As Short = &H312S '热键消息ID，此值固定，不能修改
+    Public Const MOD_ALT As Short = &H1S  'ALT按键ID
+    Public Const MOD_CONTROL As Short = &H2S  'Ctrl
+    Public Const MOD_SHIFT As Short = &H4S  'Shift
+    Public uVirtKey1, Modifiers, idHotKey As Integer
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextName.Text = Environment.MachineName '获取计算名称
+        idHotKey = 1 '注册的热键的消息ID，这个值可以随便定义，只要与下面两个参数对应即可
+        Modifiers = MOD_SHIFT '辅助键为Shift
+        '注册热键
+        RegisterHotKey(Me.Handle.ToInt32, idHotKey, Modifiers, Keys.Q) '注册的热键为Shift+Q
+        RegisterHotKey(Me.Handle.ToInt32, 2, MOD_ALT, Keys.Q) '注册的热键为Ctrl+Q。这里和上步一样，我把参数直接写了，没有先赋值，相信大家都明白
+    End Sub
+    '窗体的消息处理函数
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If m.Msg = WM_HOTKEY Then '判断是否为热键消息
+            Select Case m.WParam.ToInt32 '判断热键消息的注册ID
+                Case 1
+                    Button4_Click(Nothing, Nothing) 'Shift+Q
+                Case 2
+                    Button3_Click(Nothing, Nothing) 'Ctrl+Q
+            End Select
+        End If
+        MyBase.WndProc(m) '循环监听消息
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click '隐藏或显示窗口
+        If ShowOrHide = 1 Then
+            Me.Show()
+            ShowOrHide = 0
+        ElseIf ShowOrHide = 0 Then
+            Me.Hide()
+            ShowOrHide = 1
+        End If
+    End Sub
+
+    Private Sub ButListen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButListen.Click '开始监听的按钮
+        TextBox.CheckForIllegalCrossThreadCalls = False '取消文本框的跨线程检查
+        Dim Address As IPAddress = IPAddress.Parse(TxtIP.Text.Trim)
+        Dim EndPoint As New IPEndPoint(Address, TxtPort.Text) '创建一个网络节点对象
+        ListenSocket.Bind(EndPoint) '给负责监听的套接字绑定一个网络节点
+        ListenSocket.Listen(2)
+        ShowRecMsg("正在监听……")
+        Dim thre As New Thread(AddressOf Connect) '创建一个新的线程用于处理客户端发来的连接请求
+        thre.IsBackground = True '设为后台线程
+        thre.Start() '开启线程
+    End Sub
+
+    Sub Connect() '处理客户端的连接请求的过程
+        While True
+            Dim SockConect As Socket = ListenSocket.Accept
+            ShowRecMsg("连接成功！" & "（连接信息：" & SockConect.RemoteEndPoint.ToString & "）")
+            Dict.Add(SockConect.RemoteEndPoint.ToString, SockConect) '将连接成功的套接字添加到键值对集合
+            LBOnLine.Items.Add(SockConect.RemoteEndPoint.ToString) '添加到列表
+            Dim Thre As New Thread(AddressOf RecClient) '创建一个新的线程用于和链接成功的套接字通信
+            Thre.IsBackground = True '设为后台线程
+            Thre.Start(SockConect)
+            DictThre.Add(SockConect.RemoteEndPoint.ToString, Thre) '将创建的通信线程添加到键值对集合
+        End While
+    End Sub
+
+    Sub RecClient(ByVal SockTelNet As Socket) '处理客户端发来的数据
+        While True
+            Dim AryMsg(1024) As Byte
+            Dim RecLen As Int32
+
+            Try '捕获异常
+                RecLen = SockTelNet.Receive(AryMsg) '接受客户端发来得信息
+            Catch ss As SocketException
+                ShowRecMsg(ss.NativeErrorCode & vbCrLf & ss.Message) '显示错误信息
+                Dict.Remove(SockTelNet.RemoteEndPoint.ToString) '移除断开连接的套接字
+                LBOnLine.Items.Remove(SockTelNet.RemoteEndPoint.ToString) '从列表中移除
+                Return
+            Catch s As Exception
+                ShowRecMsg(s.Message)
+                Return
+            End Try
+
+            Dim StrMsg As String
+            StrMsg = System.Text.Encoding.UTF8.GetChars(AryMsg, 0, RecLen)
+            ShowRecMsg(StrMsg)
+            TxtMsg.ScrollToCaret()
+            Button4.PerformClick()
+        End While
+    End Sub
+
+    Sub ShowRecMsg(ByVal Msg As String) '显示接收信息绿色
+        TxtMsg.SelectionColor = Color.Green
+        TxtMsg.AppendText(Format(DateTime.Now, "F") & vbCrLf & "   " & Msg & vbCrLf) 'TxtMsg用途显示消息记录
+    End Sub
+    Sub ShowSendMsg(ByVal Msg As String) '显示接收信息红色
+        TxtMsg.SelectionColor = Color.Red
+        TxtMsg.AppendText(Format(DateTime.Now, "F") & vbCrLf & "   " & Msg & vbCrLf) 'TxtMsg用途显示消息记录
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click 'Button2群发的按钮
+        For Each a In Dict.Values '遍历通信套接字键值对集合，群发消息
+            Dim AryMsg() = System.Text.Encoding.UTF8.GetBytes(TextName.Text & " Say: " & TxtSendMsg.Text.Trim)
+            Try '捕获异常
+                a.Send(AryMsg) '发送消息
+            Catch ss As SocketException
+                ShowRecMsg(ss.NativeErrorCode & vbCrLf & ss.Message)
+            Catch s As Exception
+                ShowRecMsg(s.Message)
+            End Try
+        Next
+        ShowSendMsg(TextName.Text & " Say: " & TxtSendMsg.Text.Trim)
+        TxtSendMsg.Clear()
+        TxtMsg.ScrollToCaret()
+    End Sub
+
+    Private Sub TxtSendMsg_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtSendMsg.KeyPress '判断按确定键自动发出
+        If e.KeyChar = ChrW(13) Then
+            Me.Button2.PerformClick()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.TopMost = False Then
+            Me.TopMost = True
+        ElseIf Me.TopMost = True Then
+            Me.TopMost = False
+        End If
+    End Sub
+    '以下是窗口通知闪烁，True是代表前台闪烁，FALSE代表后台闪烁
+    Declare Function FlashWindow Lib "user32" Alias "FlashWindow" (ByVal hwnd As Long, ByVal bInvert As Long) As Long
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.FlashWindow(Me.Handle, False)
+    End Sub
+
+End Class
